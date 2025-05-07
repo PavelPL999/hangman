108b4c2cde38958a31389e629a5abc638b69a563
pavel999 <odinrg999@gmail.com>
2025-05-07 17:20:30 +0300

    #2_fix dictionary, added method structure

--- src/main/java/HangmanApp.java
+++ src/main/java/HangmanApp.java
@@ -1,7 +1,47 @@
+// Допущения:
+// без ООП
+
 public class HangmanApp {
 
     public static int mistakeCount = 0;
     public static int lifeCount = 6;
+
+    public static void main(String[] args) {
+        printHangman();
+    }
+    // Игровой процесс:
+    // рандомно выбирается из dictionary.txt и выводится на консоль слово под маской (*****)
+    // печатается сообщение - Выберите букву и игрок вводит букву
+    // далее выводится сообщение о кол-ве ошибок/жизней и хангман
+    // цикл повторяется до тех пор, пока не будет угадано полностью слово или полностью хангман
+
+    // TODO: сначала пишем структуры функций, без деталей кода
+
+    //ф-ция gameLoop() - в ней будет повторятся цикл, пока игра не закончилась
+    public static void gameLoop() {
+        // while (gameNotOver)
+        //  * вывод загаданного слова (маска, буквы)
+        //  * playerTurn: запрашиваем у человека ввод из консоли, проверяем его (д/б быть введена одна буква, не числа, не знаки)
+        //  * checkGameState (player_WON, player_LOSS, GAME_NOT_OVER)
+        //  * вывод счета и хангмана -> next round or game over
+    }
+
+    public static void makePlayerTurn() {
+        // get input
+        // validate input
+        // открывает букву/-ы или выводит сообщение, что такой буквы нет
+    }
+
+    public static void checkGameState() {
+        // Что такое победа? Когда в маске не осталось ни одной *, т.е. все буквы открыты Игроком, при этом
+        // количество допущенных ошибок mistakeCount <= 6 и осталась хотя бы одна жизнь lifeCount >= 0 -> player_WON
+        // mistakeCount > 6, lifeCount < 0, остались * в маске слова -> player_LOSS
+
+        // Какие проверки делаем:
+        // if (mistakeCount <= 6 && lifeCount >= 0 && !word.contains('*')) -> GAME_NOT_OVER
+        // if (mistakeCount > 6 && lifeCount < 0 && word.contains('*')) -> player_LOSS
+        // if (mistakeCount <= 6 && lifeCount >= 0 && word.contains('*')) -> GAME_NOT_OVER
+    }
 
     public static void printHangman() {
         if (mistakeCount == 0 && lifeCount == 6) {
@@ -82,9 +122,7 @@ public class HangmanApp {
 
     }
 
-    public static void main(String[] args) {
-        printHangman();
-    }
+
 
 
 }
